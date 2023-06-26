325ff1d1f0750acf9f63a8e0ff50187ab433c3a2
avsongco <markallensongco@gmail.com>
2023-06-26 20:10:51 +0800

    added lakip

--- partners/urls.py
+++ partners/urls.py
@@ -1,5 +1,6 @@
 from django.conf import settings
 from django.conf.urls import url
+from django.views.generic import TemplateView
 
 from . import views
 
@@ -8,6 +9,7 @@ urlpatterns = [
     url(r'^partners/(?P<partner_name>[a-zA-Z\d-]+)/$', views.PartnerView, name='partner'),
     url(r'^partners/(?P<partner_name>[a-zA-Z\d-]+)/course/{}$'.format(settings.COURSE_ID_PATTERN), views.PartnerCourseView, name='partner-course'),
     url(r'^partners/(?P<partner_name>[a-zA-Z\d-]+)/expert/(?P<expert_id>\d+)$', views.ExpertView, name='expert'),
+    url(r'^lakip/$', TemplateView.as_view(template_name="lakip-landing.html"), name='lakip'),
 ]
 
 #  from django.conf import settings
